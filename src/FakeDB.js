e67b653c3725be947f26e966f6a2c9e26c1b46a9
chamabreu <janmanuelbrenner@gmail.com>
2020-11-02 08:18:28 +0100

    fakedb export

---
+++ src/FakeDB.js
@@ -0,0 +1,98 @@
+module.exports = {
+  users: {
+    mamaID: {
+      name: "Mama",
+      pages: ["housingID"]
+    },
+  },
+
+  pages: {
+    animalsID: {
+      name: "Animals",
+      pages: ["lionID", "catID"]
+    },
+    lionID: {
+      name: "Lion",
+      pages: [],
+    },
+    catID: {
+      name: "Cat",
+      pages: [],
+    },
+    housingID: {
+      name: "Housing",
+      pages: ['bedID', "officeID"],
+    },
+    bedID: {
+      name: "Bed",
+      pages: ["manuelID", "sarahID"]
+    },
+    officeID: {
+      name: "Office",
+      pages: ["computerID", "speakersID"]
+    },
+    computerID: {
+      name: "Computer",
+      pages: []
+    },
+    speakersID: {
+      name: "Speaker",
+      pages: []
+    },
+    manuelID: {
+      name: "Manuel",
+      pages: ['lightsID']
+    },
+    sarahID: {
+      name: "Sarah",
+      pages: []
+    },
+    lightsID: {
+      name: "Lights",
+      pages: []
+    },
+    waterID: {
+      name: "Water",
+      pages: []
+    }
+  },
+
+  contents: {
+    animalsID: {
+      title: "Hi this is Animals Page",
+      floatText: "Animlas akslanknoe alk vklavdalo a lrga oöpv daklgaöf opöa gklakl adoög elka vkdanvoöpad"
+    },
+    lionID: {
+      title: "A Lion Appears",
+      floatText: "ROAAAARRR"
+    },
+    catID: {
+      title: "A Cat is nearly a Lion",
+      floatText: "MEAOUW"
+    },
+    housingID: {
+      title: "Housing",
+      floatText: "gdahkgdanökndaihgoaeng  üakpäf kaäp apä käpa ka"
+    },
+    bedID: {
+      title: "Bed",
+      floatText: "agpääpqpiwqoptoepq iot qepü iqüi oq q"
+    },
+    officeID: {
+      title: "Office",
+      floatText: "ncmbyxbnmc,xny.,xmn.<my-. ,.-m,<m,.m.,"
+    },
+    manuelID: {
+      title: "Manuel",
+      floatText: "Manuelfnakngaölgnadlngkadnpeona 456841231"
+    },
+    sarahID: {
+      title: "Sarah",
+      floatText: "/*/-*/-*/-//-/-+/-*/+-/-/*-+/-*+/-*/*-/-+*/-/*-/+-"
+    },
+    lightsID: {
+      title: "Lights",
+      floatText: "78503105093170953108679031759318780743194ß13z0"
+    }
+  }
+}
